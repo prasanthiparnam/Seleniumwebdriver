e562168d370c1d880683c6cd69149fd9e8e89933
prasanthi <prasanthi.parnam@gmail.com>
2025-11-07 11:37:47 +0530

    confirm

--- src/test/java/day24/Xpath_Demo.java
+++ src/test/java/day24/Xpath_Demo.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Xpath_Demo {
+	public class Xpath_Demo {
 
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
